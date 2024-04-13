e0250ea33dbe18716ca7bc76a2000e5306777c7d
V3NTYY <165188660+V3NTYY@users.noreply.github.com>
2024-04-13 11:05:43 -0500

    Fixed fullscreen

--- app/src/main/java/com/example/GymQuest/View/MainActivity.kt
+++ app/src/main/java/com/example/GymQuest/View/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.GymQuest.View
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -25,6 +26,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 
         firebaseRepository = FirebaseRepository()
 
