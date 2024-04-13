5e9f2b6f21f0e9d7fff9e9b7ff06111e6abdebce
cphinric <parkerhinrichs914@gmail.com>
2024-04-13 08:30:52 -0500

    Connected player statistics to main screen

--- app/src/main/java/com/example/GymQuest/View/ClaudeAPI.kt
+++ app/src/main/java/com/example/GymQuest/View/ClaudeAPI.kt
@@ -1,6 +1,5 @@
 package com.example.GymQuest.View
 
-import ApiKeys
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -9,6 +8,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
+import com.example.GymQuest.ApiKeys
 import com.example.GymQuest.R
 
 class ClaudeAPI : AppCompatActivity(){
