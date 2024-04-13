2e12c6689f9a68edf2724436fb41e80dc4734b18
cphinric <parkerhinrichs914@gmail.com>
2024-04-12 23:20:16 -0500

    Recyclerview added

---
+++ app/src/main/java/com/example/GymQuest/View/DietActivity.kt
@@ -0,0 +1,14 @@
+package com.example.GymQuest.View
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.GymQuest.R
+
+class DietActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivity_diet)
+    }
+}
