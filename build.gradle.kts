65c232794132029b20e8acc207ec15204aa7a09d
porkchop <hfountai@uark.edu>
2024-04-13 01:08:30 -0500

    New Alternatives!

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,7 @@ buildscript {
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+
+    // Add the dependency for the Google services (for Auth)
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
