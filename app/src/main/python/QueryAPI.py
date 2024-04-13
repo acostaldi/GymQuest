ba9a49572acff7310db736236c634a57055e9a41
AlexJPrill <alex.j.prill@gmail.com>
2024-04-13 11:18:37 -0500

    Imporved promt output

--- app/src/main/python/QueryAPI.py
+++ app/src/main/python/QueryAPI.py
@@ -72,8 +72,8 @@ def query_fitness_API(API_KEY):
     data = {
         "model": "claude-3-opus-20240229",
         "max_tokens": 2000,
-        "temperature": 1,
-        "system": "You are an AI assistant acting like a wizard, your job is to add brief one liner flavor texts to exercises (for example do 10 curls to train your sword arm)",
+        "temperature": 0.7,
+        "system": "You are an AI assistant acting like a wizard, your job is to add brief one liner flavor texts to exercises (for example do 10 curls to train your sword arm) these could also be something that you do on a quest",
         "messages": [
             {"role": "user",
             "content": "Output a numbered 1 - 5 comma delimited list of exercises(30 Bicep Curls, 30 Pushups, 2 Miles of Walking, and 2 Minutes of stretching) in a mix of flavor of the following prompts Monster Hunt: Describe a fierce creature that the hero must track down and defeat to progress on their quest. What unique abilities or weaknesses does the monster possess? Guardian Duel: Detail a confrontation between the hero and a powerful guardian who protects a sacred artifact or passage. How does the hero outsmart or overpower their opponent? Rescue Mission: Create a scenario where the hero must rescue a group of captives held by an evil force. What obstacles must they overcome to free the prisoners and ensure their escape to safety? Ancient Ruins Exploration: Send the hero into ancient ruins or a forbidden temple in search of a hidden treasure or lost knowledge. What traps, puzzles, or guardians block their path? Wilderness Survival: Describe a journey through treacherous wilderness where the hero must navigate harsh terrain, evade dangerous creatures, and find sustenance to continue their quest. Underworld Descent: Task the hero with descending into the underworld or a dark realm to retrieve a vital artifact or rescue a lost soul. What horrors and challenges await them in the depths? Elemental Trials: Present a series of elemental challenges (earth, fire, water, air) that the hero must overcome to prove their mastery and gain access to a hidden sanctuary or power source. Legendary Beast Taming: Introduce a legendary creature that the hero must tame or ally with to gain its aid in their quest. How does the hero earn the creature's trust and loyalty? Cursed Relic Purification: Describe a cursed relic or artifact that the hero must purify or destroy to prevent it from falling into the wrong hands. What sacrifices or rituals are required to cleanse the object?"}
@@ -100,9 +100,3 @@ def extract_text(json_string):
 
 
 
-API_KEY = "Something as of right now IDK what it is exactly"
-
-
-index = 0
-
-print(extract_text(query_API(API_KEY, prompts[index])))
