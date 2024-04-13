685ff70e0bdb786e8518472c051afa5ad0eafdb1
cphinric <parkerhinrichs914@gmail.com>
2024-04-13 12:56:28 -0500

    Connected player statistics to main screen

--- app/src/main/java/com/example/GymQuest/View/LoginActivity.kt
+++ app/src/main/java/com/example/GymQuest/View/LoginActivity.kt
@@ -47,13 +47,6 @@ class LoginActivity : AppCompatActivity() {
             signInWithGoogle()
         }
 
-        val claude = findViewById<Button>(R.id.button3)
-
-        claude.setOnClickListener {
-            val intent = Intent(this, ClaudeAPI::class.java)
-            startActivity(intent)
-        }
-
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
