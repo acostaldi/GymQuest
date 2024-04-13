1c60bbe656f7a9da8d9937cee284665aaa9f342e
porkchop <hfountai@uark.edu>
2024-04-13 06:51:52 -0500

    New Alternatives!

--- app/src/main/java/com/example/GymQuest/View/CreatePlayer.kt
+++ app/src/main/java/com/example/GymQuest/View/CreatePlayer.kt
@@ -1,5 +1,6 @@
 package com.example.GymQuest.View
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -8,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.GymQuest.Model.FirebaseRepository
 import com.example.GymQuest.R
+import com.google.firebase.auth.FirebaseAuth
 
 class CreatePlayer : AppCompatActivity(){
 
@@ -42,5 +44,21 @@ class CreatePlayer : AppCompatActivity(){
             startActivity(intent)
         }
     }
+    companion object {
+        fun getLaunchIntent(from: Context) = Intent(from, CreatePlayer::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+    }
 
+    private fun initializeUI() {
+        //val sign_out_button = findViewById<Button>(R.id.loginButton)
+        //sign_out_button.setOnClickListen {
+        //    logout()
+        //}
+    }
+
+    private fun logout() {
+        //startActivity(LoginActivity.getLaunchIntent(this))
+        //FirebaseAuth.getInstance().signOut()
+    }
 }
