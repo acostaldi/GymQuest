328f792bd4b34070b026ac81d7dd9c328fae9380
cphinric <parkerhinrichs914@gmail.com>
2024-04-13 00:04:43 -0500

    fixed database

--- app/src/main/java/com/example/GymQuest/Model/ClaudeCallsTest.kt
+++ app/src/main/java/com/example/GymQuest/Model/ClaudeCallsTest.kt
@@ -1,17 +1,17 @@
-
-
-fun fridge(val input: String)
-{
-    val KlaudeClient = KlaudeCLient.Builder()
-        .key("CLAUDE_API_KEY")
-        .model("claude-3")
-        .maxTokensToSample(1000)
-        .build()
-
-    val text = input
-    KlaudeClient.complete($text)
-    {
-        result -> print(result ?: [empty])
-    }
-
-}
+//
+//
+//fun fridge(val input: String)
+//{
+//    val KlaudeClient = KlaudeCLient.Builder()
+//        .key("CLAUDE_API_KEY")
+//        .model("claude-3")
+//        .maxTokensToSample(1000)
+//        .build()
+//
+//    val text = input
+//    KlaudeClient.complete($text)
+//    {
+//        result -> print(result ?: [empty])
+//    }
+//
+//}
--- app/src/main/java/com/example/GymQuest/Model/FirebaseRepository.kt
+++ app/src/main/java/com/example/GymQuest/Model/FirebaseRepository.kt
@@ -1,4 +1,27 @@
 package com.example.GymQuest.Model
 
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
 class FirebaseRepository {
+
+    private val db = FirebaseFirestore.getInstance()
+    private val patientsCollection = db.collection("users")
+
+    fun addPlayer(name: String) {
+        val patientDocRef = patientsCollection.document(name)
+
+        val patientData = hashMapOf(
+            "name" to name
+        )
+
+        patientDocRef.set(patientData)
+            .addOnSuccessListener {
+                println("Patient document created/updated in Firestore for user: $name")
+            }
+            .addOnFailureListener { e ->
+                println("Error creating/updating patient document in Firestore: $e")
+            }
+    }
 }
