ee01c1c429e58f5cb390cf71cb8e8d2f40d0750f
cphinric <parkerhinrichs914@gmail.com>
2024-05-15 09:30:40 -0500

    Connected player statistics to main screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
                 install("requests")
             }
 
-            buildPython("C:\\Program Files (x86)\\Microsoft Visual Studio\\Shared\\Python39_64python.exe")
+            buildPython("/usr/bin/python")
         }
     }
 
